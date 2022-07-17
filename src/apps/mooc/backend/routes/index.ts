e71356ac4d64b6ac261a223ef791647d418f6cd6
Edgar Gago Carrillo <edgargc.upc@gmail.com>
2022-07-17 12:01:45 +0200

    feat(Courses): express-validator implemention for register a route

--- src/apps/mooc/backend/routes/index.ts
+++ src/apps/mooc/backend/routes/index.ts
@@ -1,5 +1,7 @@
-import { Router } from 'express';
+import { Router, Response, Request, NextFunction } from 'express';
+import { ValidationError, validationResult } from 'express-validator';
 import glob from 'glob';
+import httpStatus from 'http-status';
 
 export function registerRoutes(router: Router) {
   const routes = glob.sync(__dirname + '/**/*.route.*');
@@ -9,4 +11,15 @@ export function registerRoutes(router: Router) {
 function register(routePath: string, router: Router) {
   const route = require(routePath);
   route.register(router);
+}
+
+export function validateReqSchema(req: Request, res: Response, next: NextFunction) {
+  const validationErrors = validationResult(req);
+  if (!validationErrors.isEmpty()) {
+    next();
+  }
+
+  const errors = validationErrors.array().map((error: ValidationError) => ({ [error.param]: error.msg }));
+
+  return res.status(httpStatus.UNPROCESSABLE_ENTITY).json({ errors });
 }
